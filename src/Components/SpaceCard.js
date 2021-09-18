bf5f708639697d6b43a7fd5c8873fba11dbffb3a
natekcroteau <natekcroteau@gmail.com>
2021-09-18 10:16:50 -0600

    displaying spacecard components with dynamic user liking functional, minimal styling

--- src/Components/SpaceCard.js
+++ src/Components/SpaceCard.js
@@ -1,10 +1,41 @@
+import { useState } from 'react'
+import moment from 'moment'
+import { RiChatHeartLine, RiChatHeartFill } from 'react-icons/ri'
 
 
-export default function SpaceCard(){
+export default function SpaceCard(props){
+
+    const [liked, setLiked] = useState(false)
+
+
+    const {
+        date,
+        title,
+        explanation,
+        imageURL
+    } = props
+
+
+    function dateFormatting(startDate){
+        return moment(startDate).format("MMMM Do, YYYY")
+    }
+
+    const ifLike = () => liked ? <RiChatHeartFill className="liked" /> : <RiChatHeartLine className="not-liked" />
+
+    function userLiking(){
+        setLiked(!liked)
+    }
 
     return(
         <div className="space-card">
-            <h2>{title}</h2>
+            <img className="card-image" src={imageURL} alt={title}/>
+            <h2 className="card-title">{title}</h2>
+            <div className ="card-date-like">
+                <span className="card-date">{dateFormatting(date)}</span>
+                <button onClick={userLiking}>{ifLike()}</button>
+            </div>
+            <p className="card-explanation">{explanation}</p>
         </div>
     )
 }
+
--- src/Components/SpaceContainer.js
+++ src/Components/SpaceContainer.js
@@ -1,22 +1,43 @@
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
+import SpaceCard from "./SpaceCard"
 
 export default function SpaceContainer(){
 
 
+    const [allSpaceData, setAllSpaceData] = useState([])
 
-    function fetchSpaceData(){
-        fetch('https://api.nasa.gov/planetary/apod?count=100&api_key=oBF3IioeqKii8OZk3tCP2JztCFs30aihttAzlrMc')
+
+    function fetchSpaceDataToState(){
+        fetch('https://api.nasa.gov/planetary/apod?count=20&api_key=oBF3IioeqKii8OZk3tCP2JztCFs30aihttAzlrMc')
             .then(response => response.json())
-            .then(spaceData => {
-                console.log(spaceData)
-            })
+            .then(spaceData => setAllSpaceData(spaceData))
     }
-    fetchSpaceData()
+    
+
+    function displaySpaceCards(){
+        if(allSpaceData){
+            return allSpaceData.map(spaceData => {
+                return <SpaceCard 
+                            key={spaceData.date}
+                            date={spaceData.date}
+                            title={spaceData.title}
+                            explanation={spaceData.explanation}
+                            imageURL={spaceData.url}
+                        />
+            })
+        }else{
+            return null
+        }
+    }
+
+    useEffect(fetchSpaceDataToState, [])
+    // useEffect(displaySpaceCards, [allSpaceData])
+
 
     return(
         <div className="space-container">
-
+            {displaySpaceCards()}
         </div>
     )
+    
 }
